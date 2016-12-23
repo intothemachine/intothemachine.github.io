650db5aa536d61eb2582bb3da6b0942c6908c07a
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2016-12-23 17:44:20 +0100

    add random posts data for testing

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const Posts = [{
+      title: 'Random post1 for testing.',
+      categorie: ['React.js', 'Node.js', 'express','Redux'],
+      content: 'Random content, I hate my life, why am i here, I hate everything. What is Life, is it a cruel joke by God... ok I got it ur funny now end it, please.'
+      
+},
+{
+      title: 'Random post2 for testing.',
+      categorie: ['React.js', 'Node.js', 'express','Redux'],
+      content: 'Random content, I hate my life, why am i here, I hate everything. What is Life, is it a cruel joke by God... ok I got it ur funny now end it, please.'
+      
+},
+{
+      title: 'Random post3 for testing.',
+      categorie: ['React.js', 'Node.js', 'express','Redux'],
+      content: 'Random content, I hate my life, why am i here, I hate everything. What is Life, is it a cruel joke by God... ok I got it ur funny, now end it, please.'
+      
+}
+];
+export {Posts};
