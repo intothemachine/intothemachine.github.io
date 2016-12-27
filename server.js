573e8db1ea9fe88ecc21f5c70b66f7b5eba804d0
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2016-12-27 14:26:29 +0100

    implement routes for get,put,delete post by id

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ var POSTS_COLLECTION = "posts";
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
-console.log(process.env.MONGOLAB_URI);
 if(process.env.MONGOLAB_URI){
 mongodb.MongoClient.connect(process.env.MONGOLAB_URI,  (err, database) => {
   if (err) {
@@ -33,6 +32,11 @@ app.set('port', (process.env.PORT || 3001));
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
+
+/*  "/postss"
+ *    GET: finds all posts
+ *    POST: creates a new post
+ */
 
 app.get('/api/posts', (req, res) => {
 	console.log("inside the get request");
@@ -60,6 +64,45 @@ app.post("/api/posts", (req, res) =>{
       handleError(res, err.message, "Failed to create new contact.");
     } else {
       res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+/*  "/posts/:id"
+ *    GET: find post by id
+ *    PUT: update post by id
+ *    DELETE: deletes post by id
+ */
+
+app.get("/posts/:id", function(req, res) {
+  db.collection(POSTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get Post");
+    } else {
+      res.status(200).json(doc);  
+    }
+  });
+});
+
+app.put("/posts/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(POSTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update post");
+    } else {
+      res.status(204).end();
+    }
+  });
+});
+
+app.delete("/posts/:id", function(req, res) {
+  db.collection(POSTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete Post");
+    } else {
+      res.status(204).end();
     }
   });
 });
